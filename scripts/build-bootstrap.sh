02ae6a23d532c7339283be77d5d31e4406dda16f
jzinferno <jzinferno@gmail.com>
2024-08-18 22:08:28 +0300

    scripts: Update build-bootstrap.sh

--- scripts/build-bootstrap.sh
+++ scripts/build-bootstrap.sh
@@ -3,52 +3,51 @@
 scripts_dir="$(cd "$(dirname "$0")" && pwd)"
 
 create_prefix() {
-    rm -rf $TERMUX_PREFIX
-    cp -r "$scripts_dir"/../root $TERMUX_PREFIX
+  rm -rf $TERMUX_PREFIX
+  cp -r "$scripts_dir"/../root $TERMUX_PREFIX
 }
 
 build_packages() {
-    "$scripts_dir/../packages/ncurses/build.sh"
-    "$scripts_dir/../packages/readline/build.sh"
-    "$scripts_dir/../packages/bash/build.sh"
-    "$scripts_dir/../packages/nano/build.sh"
+  for base_pkgs in ncurses readline bash nano; do
+    "$scripts_dir/../packages/${base_pkgs}/build.sh"
+  done
 }
 
 reduce_size() {
-    cd $TERMUX_PREFIX
-    for library in $(find ./lib -name "*.a" -o -name "*.so*"); do
-        llvm-strip --strip-unneeded $library
-    done
-    for binary in $(find ./bin); do
-        file $binary | grep "not stripped" >/dev/null
-        if [ $? -eq 0 ]; then
-            llvm-strip --strip-unneeded $binary
-        fi
-    done
+  cd $TERMUX_PREFIX
+  for library in $(find ./lib -name "*.a" -o -name "*.so*"); do
+    llvm-strip --strip-unneeded $library
+  done
+  for binary in $(find ./bin); do
+    file $binary | grep "not stripped" >/dev/null
+    if [ $? -eq 0 ]; then
+      llvm-strip --strip-unneeded $binary
+    fi
+  done
 }
 
 generate_bootstrap() {
-    cd $TERMUX_PREFIX
-    while read -r -d '' link; do
-        echo "$(readlink "$link")←${link}" >> SYMLINKS.txt
-        rm -f "$link"
-    done < <(find . -type l -print0)
+  cd $TERMUX_PREFIX
+  while read -r -d '' link; do
+    echo "$(readlink "$link")←${link}" >> SYMLINKS.txt
+    rm -f "$link"
+  done < <(find . -type l -print0)
 
-    zip -r9 "bootstrap-${architecture}.zip" ./*
-    mv "bootstrap-${architecture}.zip" "$scripts_dir/.."
+  zip -r9 "bootstrap-${architecture}.zip" ./*
+  mv "bootstrap-${architecture}.zip" "$scripts_dir/.."
 }
 
 generate_checksums() {
-    cd "$scripts_dir/.."
-    sha256sum bootstrap-* > CHECKSUMS-sha256.txt
+  cd "$scripts_dir/.."
+  sha256sum bootstrap-* > CHECKSUMS-sha256.txt
 }
 
 main() {
-    create_prefix
-    build_packages
-    reduce_size
-    generate_bootstrap
-    generate_checksums
+  create_prefix
+  build_packages
+  reduce_size
+  generate_bootstrap
+  generate_checksums
 }
 
 export TERMUX_PREFIX=/data/data/com.jzinferno.termux/files/usr
